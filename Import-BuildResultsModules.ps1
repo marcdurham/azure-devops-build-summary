9cb2f6bccf2c1b1b3d495168aaa058853f83080b
Marc Durham <marcdurham@gmail.com>
2020-03-30 21:06:05 -0700

    Delete reference to deleted file

--- Import-BuildResultsModules.ps1
+++ Import-BuildResultsModules.ps1
@@ -3,6 +3,5 @@
 Import-Module $moduleFolder\Invoke-WebRequestAuthorized.psm1 -Force
 Import-Module $moduleFolder\Get-BuildTimeline.psm1 -Force
 Import-Module $moduleFolder\Get-BuildList.psm1 -Force
-Import-Module $moduleFolder\Get-BuildInfoFromBuildId.psm1 -Force
 Import-Module $moduleFolder\Get-BuildResults.psm1 -Force
 Import-Module $moduleFolder\Get-BuildResultsFormatted.psm1 -Force
